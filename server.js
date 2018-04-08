f79b8b6d592e96c61b5c9f008bdefc315cf3cd97
xiaosong <kunkkaco@gmail.com>
2018-04-08 16:57:08 +0800

    fix: Access-Control-Allow-Headers

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, SessionToken, Content-Type, Accept');
   next();
 });
 
